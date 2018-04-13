d6881d13d278c3a504101df4866c7ad67b73c43f
Bertware <bert.marcelis@ugent.be>
2018-04-13 14:17:10 +0200

    Created benchmark classes to run benchmarks and tests in order to compare API performance

---
+++ Hyperrail/src/androidTest/java/be/hyperrail/android/test/ApiLogfileComparisonBenchmark.java
@@ -0,0 +1,29 @@
+package be.hyperrail.android.test;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import java.time.LocalTime;
+
+import be.hyperrail.android.irail.contracts.IrailRequest;
+
+/**
+ * Compare APIs using data from iRail log files
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ApiLogfileComparisonBenchmark {
+
+    @BeforeClass
+    public void setup(){
+
+    }
+
+
+    class LogEntry {
+
+    }
+}
